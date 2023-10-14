5dcc2183ca8b8ecd235bb15269d417dd1f5f2ace
asimpson691 <62562142+asimpson691@users.noreply.github.com>
2023-10-14 14:26:46 +0100

    Initial commit

---
+++ src/components/app/app.js
@@ -0,0 +1,38 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+import { TodoLocal } from '../../services/todo-local';
+import { withStateAndDispatch } from '../../store';
+import { HeaderContainer } from '../header/header';
+import { ListContainer } from '../list/list';
+import { FooterContainer } from '../footer/footer';
+import { CopyRight } from '../copy-right/copy-right';
+
+export class App extends Component {
+  componentDidMount() {
+    this.props.onLoad(TodoLocal.loadTodos());
+  }
+
+  componentDidUpdate() {
+    TodoLocal.storeTodos(this.props.todos);
+  }
+
+  render() {
+    return (
+      <div id="app">
+        <section className="todoapp">
+          <HeaderContainer />
+          {!!this.props.todos.length && <ListContainer />}
+          {!!this.props.todos.length && <FooterContainer />}
+        </section>
+        <CopyRight />
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  todos: PropTypes.array.isRequired,
+  onLoad: PropTypes.func.isRequired
+};
+
+export const AppContainer = withStateAndDispatch(App);
